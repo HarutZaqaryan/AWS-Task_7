e92eda6112c1aa46d7ae407ae2c99ebd4847dd3a
Harut <zaqaryanharut242gmail.com>
2024-08-12 19:23:52 +0400

    Initial commit

---
+++ task07/app/lambdas/uuid_generator/index.js
@@ -0,0 +1,50 @@
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { v4 as uuidv4 } from "uuid";
+
+export const handler = async (event) => {
+  const s3Client = new S3Client();
+  const bucketName = process.env.target_bucket || "uuid-storage";
+  console.log("~~~bucketName~~~", bucketName);
+
+  // Generate 10 random UUIDs
+  const uuids = Array.from({ length: 10 }, () => uuidv4());
+  console.log("~~~uuids(10)~~~", uuids);
+
+  // Get current ISO time
+  const executionTime = new Date().toISOString();
+
+  // Prepare the file content
+  const fileContent = JSON.stringify({ ids: uuids }, null, 2);
+  console.log("~~~fileContent~~~", fileContent);
+
+  // Define the file name
+  const fileName = `${executionTime}.json`;
+  console.log("~~~fileName~~~", fileName);
+
+  // Upload the file to S3
+  try {
+    const command = new PutObjectCommand({
+      Bucket: bucketName,
+      Key: fileName,
+      Body: fileContent,
+      ContentType: "application/json",
+    });
+    console.log("~~~Put Object Command~~~", command);
+
+    await s3Client.send(command);
+
+    console.log(`File uploaded successfully to ${bucketName}/${fileName}`);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        `File uploaded successfully to ${bucketName}/${fileName}`
+      ),
+    };
+  } catch (error) {
+    console.error("Error uploading file:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify("Error uploading file"),
+    };
+  }
+};
